1d67859062e693b78f427a3004b60420e4fee329
AndreasC <andreascalvin21@gmail.com>
2025-05-15 22:55:50 +0700

    fix database, env

--- models/Database.php
+++ models/Database.php
@@ -1,6 +1,8 @@
 <?php
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
+
 class Database {
     private $host;
     private $port;
