ac8c59180f5d6b29a6481959077a9b9bde1e1a07
AndreasC <andreascalvin21@gmail.com>
2025-05-15 23:52:04 +0700

    another try to fix search

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
     let keyword = $(this).val();
     if (keyword.length >= 1) {
       $.ajax({
-        url: "/api/controllers/search.php",
+        url: "/search.php",
         method: "GET",
         data: { keyword: keyword },
         success: function (data) {
